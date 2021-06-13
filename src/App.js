0905091e3abfbbe66f093bc2c412faadbbf7d523
davidcoderistov <davidcoderistov@gmail.com>
2021-06-13 18:24:42 +0200

    Login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Login } from "./components/login";
+import { Grid } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { getAuth } from "./selectors";
 
 function App() {
+  const auth = useSelector(getAuth);
+  console.log(auth);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+      >
+        <Login/>
+      </Grid>
   );
 }
 
